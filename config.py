5361ed59708b7404d4f8fbdc465a7436e7c87899
ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>
2024-04-05 23:41:58 +0630

    Update config.py
    Signed-off-by: ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,11 +39,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/seriousvs_version20")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/seriousvs_version10")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/musicworldstech")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/musicworldschat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
